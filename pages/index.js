9551af4222e956c314952eedc39f3e53c89ab5dc
Michael Wood <michaelwood33311@icloud.com>
2023-04-01 17:21:29 -0400

    added logo to homepage

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import { useUser } from '@auth0/nextjs-auth0/client';
-
+import HeroImage from '../public/hero.webp';
+import Image from "next/image";
+import { Logo } from '../components/Logo';
 
 export default function Home() {
 
@@ -9,9 +11,13 @@ export default function Home() {
   if (typeof user !== 'undefined') console.log('user', user);
 
   return <div>
-    <div>
+    <div className="w-screen h-screen overflow-hidden flex justify-center items-center relative">
+      <Image src={HeroImage} alt="Hero" className="absolute w-screen h-screen" />
+      <div className="relative z-10 text-white px-10 py-5 text-center max-w-screen-sm bg-slate-900/90 rounded-md backdrop-blur-sm">
+        <Logo />
+      </div>
      
-      {!user && <Link href="/api/auth/login">Login</Link>}
+      {/* {!user && <Link href="/api/auth/login">Login</Link>} */}
     </div>  
   </div>;
 }
