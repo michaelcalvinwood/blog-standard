aa5230045e97b7e551ddc2f39e7665eea58dc7d8
Michael Wood <michaelwood33311@icloud.com>
2023-04-01 13:30:13 -0400

    added displaying user gravatar and email address

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { useUser } from '@auth0/nextjs-auth0/client';
+import Image from "next/image";
 
 export default function Home() {
 
@@ -10,7 +11,8 @@ export default function Home() {
   return <div>
     <div>
       {user && <div>
-        Hello {user.email}<br />
+        <Image src={user.picture} alt={user.name} height={50} width={50} />
+        {user.email}<br />
         <Link href="/api/auth/logout">Logout</Link>
         </div>
       }
