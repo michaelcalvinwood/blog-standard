204facfc30653bde1ab8c81f5d99f8792994fa7b
Michael Wood <michaelwood33311@icloud.com>
2023-04-01 17:00:56 -0400

    added Logo component shell

--- components/AppLayout/AppLayout.js
+++ components/AppLayout/AppLayout.js
@@ -3,7 +3,7 @@ import { useUser } from '@auth0/nextjs-auth0/client';
 import Image from "next/image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCoins } from "@fortawesome/free-solid-svg-icons";
-
+import { Logo } from "../Logo";
 
 export const AppLayout = ({children}) => {
     const {user} = useUser();
@@ -11,7 +11,7 @@ export const AppLayout = ({children}) => {
         <div className="grid grid-cols-[300px_1fr] h-screen max-h-screen">
             <div className='flex flex-col text-white overflow-hidden'>
                 <div className="bg-slate-800 px-2">
-                    <div>Logo</div>
+                    <Logo />
                     <Link 
                         href="/post/new" 
                         className="bg-green-500 tracking-wider w-full text-center text-white cursor-pointer uppercase px-4 py-2 rounded-md font-bold hover:bg-green-600 transition-colors block"
