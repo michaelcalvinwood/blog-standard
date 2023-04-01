59c7107fedefcdd82e00eae4bd0aff086af38673
Michael Wood <michaelwood33311@icloud.com>
2023-04-01 13:20:04 -0400

    added getting user information via auth0

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,15 @@
 import Link from "next/link";
+import { useUser } from '@auth0/nextjs-auth0/client';
+
 export default function Home() {
+
+  const {user} = useUser();
+
+  if (typeof user !== 'undefined') console.log('user', user);
+
   return <div>
     <div>
-      
+      Hello
       <Link href="/api/auth/login">Login</Link>
     </div>  
   </div>;
