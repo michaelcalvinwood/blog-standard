3d0edcf05642d11c0d1ec41c90422516a68b42d6
Michael Wood <michaelwood33311@icloud.com>
2023-04-01 16:37:43 -0400

    implemented the sidebar footer

--- components/AppLayout/AppLayout.js
+++ components/AppLayout/AppLayout.js
@@ -1,4 +1,10 @@
+import Link from "next/link";
+import { useUser } from '@auth0/nextjs-auth0/client';
+import Image from "next/image";
+
+
 export const AppLayout = ({children}) => {
+    const {user} = useUser();
     return (
         <div className="grid grid-cols-[300px_1fr] h-screen max-h-screen">
             <div className='flex flex-col text-white overflow-hidden'>
@@ -8,7 +14,16 @@ export const AppLayout = ({children}) => {
                     <div>tokens</div>
                 </div>
                 <div className="flex-1 overflow-auto  bg-gradient-to-b from-slate-800 to-cyan-800">List of all posts</div>
-                <div className="bg-cyan-800">User info</div>
+                <div className="bg-cyan-800">
+                {user && <div className=" flex items-center gap border-t border-t-black/50 h-20 px-2">
+                    <Image className="mr-2 rounded-full" src={user.picture} alt={user.name} height={50} width={50} />
+                    <div>
+                        <div>{user.email}</div>
+                        <Link href="/api/auth/logout">Logout</Link>
+                    </div>
+                 </div>
+                }
+                </div>
             </div>
             <div className='bg-yellow-500'>{children}</div>
         </div>
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import { useUser } from '@auth0/nextjs-auth0/client';
-import Image from "next/image";
+
 
 export default function Home() {
 
@@ -10,12 +10,7 @@ export default function Home() {
 
   return <div>
     <div>
-      {user && <div>
-        <Image src={user.picture} alt={user.name} height={50} width={50} />
-        {user.email}<br />
-        <Link href="/api/auth/logout">Logout</Link>
-        </div>
-      }
+     
       {!user && <Link href="/api/auth/login">Login</Link>}
     </div>  
   </div>;
