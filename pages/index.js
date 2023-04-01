a0acb4744deb1d9c95445b3eef86e1bca8857d17
Michael Wood <michaelwood33311@icloud.com>
2023-04-01 17:29:02 -0400

    completed homepage

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,10 @@ export default function Home() {
       <Image src={HeroImage} alt="Hero" className="absolute w-screen h-screen" />
       <div className="relative z-10 text-white px-10 py-5 text-center max-w-screen-sm bg-slate-900/90 rounded-md backdrop-blur-sm">
         <Logo />
+        <p>
+          The AI-powered SAAS solution to generate SEO-optimized blog posts in minutes. Get high-quality content, without sacrificing your time.
+        </p>
+        <Link href="/post/new" className="btn ">Begin</Link>
       </div>
      
       {/* {!user && <Link href="/api/auth/login">Login</Link>} */}
