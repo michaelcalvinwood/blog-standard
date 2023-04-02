a5566f4e4361721f925d0e67d2e0440c53cb83f8
Michael Wood <michaelwood33311@icloud.com>
2023-04-02 13:01:56 -0400

    added incrementing tokens in mongodb when Add Tokens button clicked

--- pages/api/addTokens.js
+++ pages/api/addTokens.js
@@ -1,9 +1,26 @@
-import { getSession } from "@auth0/nextjs-auth0"
+import { getSession } from "@auth0/nextjs-auth0";
+import clientPromise from '../../lib/mongodb';
 
 export default async function handler(req, res) {
     const {user} = await getSession(req, res);
 
-    console.log('user', user);
+    const client = await clientPromise;
+    const db = client.db("blog_standard");
+    const userProfile = await db.collection('users').updateOne({
+      auto0Id: user.sub
+    }, {
+      $inc: {
+        availableTokens: 10
+      },
+      $setOnInsert: {
+        auto0Id: user.sub
+      }
+    }, 
+      {
+        upsert: true
+      }
+    )
+    console.log('userProfile', userProfile);
 
     res.status(200).json({ name: 'John Doe' })
   }
