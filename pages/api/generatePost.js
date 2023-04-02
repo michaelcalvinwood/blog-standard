21da7bc56f60cb63e090467163c3063e2c7a666b
Michael Wood <michaelwood33311@icloud.com>
2023-04-02 08:35:53 -0400

    added form to allow user to input topic, keywords, and special instructions

--- pages/api/generatePost.js
+++ pages/api/generatePost.js
@@ -1,23 +1,37 @@
 import { Configuration, OpenAIApi } from 'openai';
 
 export default async function handler(req, res) {
-    console.log('handler');
-
     const config = new Configuration({
         apiKey: process.env.OPENAI_API_KEY
     });
+
+    const topic = "The ten best places to visit in Europe and why";
+    const keywords = 'European Vacation, Visit Europe';
+    const specialInstructions = 'The response should include the name of at least one restaurant, one hotel, and one tourist attraction for each place.'
 
     const openai = new OpenAIApi(config);
 
     const response = await openai.createCompletion({
         model: 'text-davinci-003',
-        temperature: 0, // 0 - 1 : how much latitude do you want the engine to have (from 0% to 100% latitude)
+        temperature: 0.25, // 0 - 1 : how much latitude do you want the engine to have (from 0% to 100% latitude)
         max_tokens: 3600, // maximum 4000 tokens for text-davinci-003
-        prompt: "Generate a blog post about owning dogs"
+        prompt: `Write a long and detailed SEO-friendly blog post about ${topic}, that targets the following comma-separated keywords: ${keywords}.
+        The content should be formatted in SEO-friendly HTML.
+        The response should include an HTML title and meta description that both include the keywords.
+        The response should include the keywords as many times as possible.
+        ${specialInstructions}
+        Also generate a list of tags that include the important words and phrases in the response. 
+        The list of tags must also include the names of all people, places, companies, and organizations mentioned in the response.
+        The return format must be stringified JSON in the following format: {
+            "postContent": post content here
+            "title": title goes here
+            "metaDescription" : meta description goes here,
+            "tags": array of tags go here
+        }`,
     });
 
-    console.log('response: ', response);
+    
 
-    res.status(200).json({post: response.data.choices})
+    res.status(200).json({post: JSON.parse(response.data.choices[0].text.replaceAll("\n", ""))})
   }
   
--- pages/post/new.js
+++ pages/post/new.js
@@ -1,21 +1,50 @@
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 import { AppLayout } from "../../components/AppLayout";
+import { useState } from "react";
 
 export default function NewPost(props) {
-    
-    const handleClick = async () => {
+    const [postContent, setPostContent] = useState('');
+    const [topic, setTopic] = useState('');
+    const [keywords, setKeywords] = useState('');
+    const [specialInstructions, setSpecialInstructions] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
         const response = await fetch(`/api/generatePost`, {
             method: 'POST'
         })
         const json = await response.json();
-        console.log('RESULT:', json);
+        console.log('RESULT:', json.post);
+        setPostContent(json.post.postContent);
     }
 
     return <div>
-        <h1>New Post Page</h1>
-        <button className="btn" onClick={handleClick}>
-            Generate
-        </button>
+        <form>
+            <div></div>
+                <label>
+                    <strong>Generate a blog post on the top of:</strong>
+                </label>
+                <textarea value={topic} onChange={(e) => setTopic(e.target.value)} />
+            <div>
+                <label>
+                    <strong>Targeting the following keywords:</strong>
+                </label>
+                <textarea value={keywords} onChange={(e) => setKeywords(e.target.value)} />
+            </div>
+            <div>
+                <label>
+                    <strong>Special Instructions (optional):</strong>
+                </label>
+                <textarea value={specialInstructions} onChange={(e) => setSpecialInstructions(e.target.value)} />
+            </div>
+            <button type='submit' className="btn" onSubmit={handleSubmit}>
+                Generate
+            </button>
+        </form>
+        <div 
+            dangerouslySetInnerHTML={{__html: postContent}}
+            className="max-w-screen-sm p-10"
+        ></div>
     </div>
   }
 
