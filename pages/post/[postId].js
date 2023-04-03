a1d9097ef4c8e154faaf728bca56fb5cb0cb100d
Michael Wood <michaelwood33311@icloud.com>
2023-04-03 13:08:42 -0400

    added displaying keywords in /post/[postId] endpoint

--- pages/post/[postId].js
+++ pages/post/[postId].js
@@ -2,6 +2,8 @@ import { getSession, withPageAuthRequired } from "@auth0/nextjs-auth0";
 import { AppLayout } from "../../components/AppLayout";
 import clientPromise from "../../lib/mongodb";
 import { ObjectId } from "mongodb";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHashtag } from "@fortawesome/free-solid-svg-icons";
 export default function Post(props) {
     console.log('Post props', props);
 
@@ -13,6 +15,16 @@ export default function Post(props) {
                     <div className="text-blue-600 text-2xl font-bold">{props.title}</div>
                     <div className="mt-2">{props.metaDescription}</div>
                 </div>
+
+                <div className="text-sm font-bold mt-6 p-2 bg-stone-200 rounded-sm">Keywords</div>
+                <div className="flex flex-wrap pt-2 gap-1">
+                    {props.keywords.split(",").map((keyword, i) => {
+                        return <div key={keyword + i} className="p-2 rounded-full bg-slate-800 text-white">
+                            <FontAwesomeIcon icon={faHashtag} /> {keyword}
+                        </div>
+                    })}
+                </div>
+
                 <div className="text-sm font-bold mt-6 p-2 bg-stone-200 rounded-sm">Blog Post</div>
                 <div dangerouslySetInnerHTML={{__html: props.postContent || ""}}></div>
             </div>
