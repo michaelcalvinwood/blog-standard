2236af04934e5860d15598b788a0680abf8aa789
Michael Wood <michaelwood33311@icloud.com>
2023-04-07 07:38:58 -0400

    created a modal for the blog inputs

--- pages/post/new.js
+++ pages/post/new.js
@@ -28,29 +28,31 @@ export default function NewPost(props) {
         }
     }
 
-    return <div>
-        <form onSubmit={handleSubmit}>
-            <div></div>
-                <label>
-                    <strong>Generate a blog post on the top of:</strong>
-                </label>
-                <textarea className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm" value={topic} onChange={(e) => setTopic(e.target.value)} />
-            <div>
-                <label>
-                    <strong>Targeting the following keywords:</strong>
-                </label>
-                <textarea className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm" value={keywords} onChange={(e) => setKeywords(e.target.value)} />
-            </div>
-            <div>
-                <label>
-                    <strong>Special Instructions (optional):</strong>
-                </label>
-                <textarea className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm" value={specialInstructions} onChange={(e) => setSpecialInstructions(e.target.value)} />
-            </div>
-            <button type='submit' className="btn" onSubmit={handleSubmit}>
-                Generate
-            </button>
-        </form>
+    return <div className="h-full overflow-hidden">
+        <div className="w-full h-full flex flex-col overflow-auto">
+            <form onSubmit={handleSubmit} className="m-auto w-full max-w-screen-sm bg-slate-100 p-4 rounded-md shadow-xl border border-slate-200 shadow-slate-200">
+                <div></div>
+                    <label>
+                        <strong>Generate a blog post on the topic of:</strong>
+                    </label>
+                    <textarea className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm" value={topic} onChange={(e) => setTopic(e.target.value)} />
+                <div>
+                    <label>
+                        <strong>Targeting the following keywords:</strong>
+                    </label>
+                    <textarea className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm" value={keywords} onChange={(e) => setKeywords(e.target.value)} />
+                </div>
+                <div>
+                    <label>
+                        <strong>Special Instructions (optional):</strong>
+                    </label>
+                    <textarea className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm" value={specialInstructions} onChange={(e) => setSpecialInstructions(e.target.value)} />
+                </div>
+                <button type='submit' className="btn" onSubmit={handleSubmit}>
+                    Generate
+                </button>
+            </form>
+        </div>
        
     </div>
   }
